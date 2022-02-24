3a63147f7c55df7cc76e36761d4e86bf9105f649
Cristi0009 <cristina-calero@hotmail.com>
2022-02-23 23:25:22 -0500

    [UPDATE] change optica for image

--- resources/views/pages/briefcase/optica_israel.blade.php
+++ resources/views/pages/briefcase/optica_israel.blade.php
@@ -12,15 +12,15 @@
               <div class="swiper-wrapper align-items-center">
 
                 <div class="swiper-slide">
-                  <img src="/assets/img/briefcase/optica/optica1.png" alt="">
+                  <img src="/assets/img/briefcase/optica/image1.png" alt="">
                 </div>
 
                 <div class="swiper-slide">
-                  <img src="/assets/img/briefcase/optica/optica1.png" alt="">
+                  <img src="/assets/img/briefcase/optica/image2.png" alt="">
                 </div>
 
                 <div class="swiper-slide">
-                  <img src="/assets/img/briefcase/optica/optica1.png" alt="">
+                  <img src="/assets/img/briefcase/optica/image3.png" alt="">
                 </div>
 
               </div>
