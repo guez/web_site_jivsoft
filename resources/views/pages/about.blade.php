967ce80e26513342f179b0b61b1d751585ca68b7
Cristi0009 <cristina-calero@hotmail.com>
2022-02-24 13:09:08 -0500

    [UPDATE] information about.blade.php

---
+++ resources/views/pages/about.blade.php
@@ -0,0 +1,52 @@
+@extends('layout')
+@section('main')
+
+<!-- ======= About Us Section ======= -->
+<section id="about" class="about">
+      <div class="container" data-aos="fade-up">
+        <div class="row no-gutters">
+          <div class="col-lg-6 video-box">
+            <img src="assets/img/about.jpg" class="img-fluid" alt="">
+            <!-- <a href="https://www.youtube.com/watch?v=jDDaplaOz7Q" class="venobox play-btn mb-4" data-vbtype="video" data-autoplay="true"></a> -->
+          </div>
+
+          <div class="col-lg-6 d-flex flex-column justify-content-center about-content">
+
+            <div class="section-title">
+              <h2>Nosotros</h2>
+              <p>Somos una empresa dedicada al desarrollo de software con más de 5 años en el mercado tecnológico, brindando soluciones web
+                creativas a empresas y personas interesadas en tener una presencia activa en internet.</p>
+                <br>
+                <p>Nuestro objetivo es proporcionar soluciones
+                  informáticas y de gestión adecuadas a cada
+                  cliente, usando tecnologías y metodologías
+                  avanzadas.</p>
+            </div>
+
+            <div class="icon-box" data-aos="fade-up" data-aos-delay="100">
+              <div class="icon"><i class="bx bx-building"></i></div>
+              <h4 class="title"><a href="">MISIÓN</a></h4>
+              <p class="description">JIVSOFT es una empresa orientada a las
+                soluciones informáticas comprometida con
+                el desarrollo de software de alta calidad, para
+                satisfacer las necesidades de pequeñas y
+                grandes empresas con software adaptable y
+                escalable incrementando su competitividad y
+                productividad.</p>
+            </div>
+
+            <div class="icon-box" data-aos="fade-up" data-aos-delay="100">
+              <div class="icon"><i class="bx bx-briefcase"></i></div>
+              <h4 class="title"><a href="">VISIÓN</a></h4>
+              <p class="description">Ser una empresa líder en el desarrollo de soluciones tecnológicas innovadoras a nivel
+                provincial y nacional cumpliendo con los estándares de calidad de software, fomentando las últimas tendencias tecnológicas y
+                lograr la satisfacción de nuestros clientes.</p>
+            </div>
+
+          </div>
+        </div>
+
+      </div>
+    </section><!-- End About Us Section -->
+
+@endsection
