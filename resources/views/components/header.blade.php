91a52eb6fbf14c8e7819c7460a50aeb961f9c757
Cristi0009 <cristina-calero@hotmail.com>
2022-02-24 13:12:55 -0500

    [UPDATE] change symbol for ablout.blade.php

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -15,10 +15,10 @@
           <li><a class="nav-link scrollto" href="/portafolio">Portfolio</a></li>
           <li><a class="nav-link scrollto" href="/services">Servicios</a></li>
           <li><a class="nav-link scrollto" href="#team">Equipo</a></li>
-          <li class="dropdown"><a href="#about"><span>Nosotros</span> <i class="bi bi-chevron-down"></i></a>
+          <li class="dropdown"><a href="#"><span>Nosotros</span> <i class="bi bi-chevron-down"></i></a>
             <ul>
-              <li><a href="#">Visión</a></li>
-              <li><a href="#">Misión</a></li>
+              <li><a href="about">Visión</a></li>
+              <li><a href="about">Misión</a></li>
               <li><a href="#">Objetivos</a></li>
             </ul>
           </li>
