6e37aab1fd55d08e83f91367f624f51ba18da663
Cristi0009 <cristina-calero@hotmail.com>
2022-02-24 20:42:04 -0500

    [UPDATE] add services section.blade.php

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -111,4 +111,73 @@
 
       </div>
     </section><!-- End Objetive Section -->
+
+    <!-- ======= Services Section ======= -->
+  <section id="services" class="services">
+      <div class="container" data-aos="fade-up">
+
+        <div class="section-title">
+          <h2>Servicios</h2>
+        </div>
+
+        <div class="row">
+          <div class="col-lg-4 col-md-6 icon-box" data-aos="fade-up">
+            <div class="icon"><i class="bi bi-globe"></i></div>
+            <h4 class="title"><a href="">Página Web</a></h4>
+            <p class="description">En la actualidad, es imprescindible que una empresa u
+              organización tenga presencia
+              en la web. JIVSoft brinda a sus clientes
+              la mejor experiencia con una
+              atractiva aplicación web,
+              desde la planificación y
+              diseño de su sitio web, hasta
+              la creación y mantenimiento
+              del mismo.
+              </p>
+              
+          </div>
+          <div class="col-lg-4 col-md-6 icon-box" data-aos="fade-up" data-aos-delay="100">
+            <div class="icon"><i class="bi bi-phone-vibrate"></i></div>
+            <h4 class="title"><a href="">Aplicaciones Móviles</a></h4>
+            <p class="description">JIVSoft te ofrece soluciones
+              profesionales, creativas, desarrolladas a medida para
+              dispositivos móviles; desde
+              el diseño conceptual, programación y publicación de las
+              aplicaciones en las correspondientes tiendas de distribución, con cuál podrás acceder a un mercado en constante crecimiento.</p>
+          </div>
+          <div class="col-lg-4 col-md-6 icon-box" data-aos="fade-up" data-aos-delay="200">
+            <div class="icon"><i class="bi bi-laptop"></i></div>
+            <h4 class="title"><a href="">Sistemas a Medida</a></h4>
+            <p class="description">JIVSoft le ofrece sistemas 100% personalizados, que cubran
+              las necesidades de cada negocio. Somos una empresa especializada en programación de plataformas y aplicaciones web
+              administrables, estables y escalables.</p>
+          </div>
+          <div class="col-lg-4 col-md-6 icon-box" data-aos="fade-up" data-aos-delay="300">
+            <div class="icon"><i class="bi bi-person-workspace"></i></div>
+            <h4 class="title"><a href="">Asesoría Tecnológica</a></h4>
+            <p class="description">Nuestros profesionales son
+              capaces de asesorarle con
+              las mejores opciones a implementar para todo tipo de
+              empresas, pymes y autónomos, que permita conseguir
+              una buena alineación entre
+              los objetivos de la organización y los sistemas a implementarse.</p>
+          </div>
+          <div class="col-lg-4 col-md-6 icon-box" data-aos="fade-up" data-aos-delay="400">
+            <div class="icon"><i class="bi bi-facebook"></i></div>
+            <h4 class="title"><a href="">Contenido para Redes Sociales</a></h4>
+            <p class="description">Desarrollamos campañas y contenidos que te permiten llegar a tu grupo 
+              objetivo. Manejamos y gestionamos las redes sociales con contenidos de valor para atraer y 
+              generar nuevos seguidores.</p>
+          </div>
+          <div class="col-lg-4 col-md-6 icon-box" data-aos="fade-up" data-aos-delay="500">
+            <div class="icon"><i class="bi bi-pencil-square"></i></div>
+            <h4 class="title"><a href="">Creación de Identidad Visual</a></h4>
+            <p class="description">Desarrollamos marcas con personalidad, conceptos creativos a la 
+              vanguardia del diseño y la funcionalidad, alineados a tus objetivos para un rápido 
+              posicionamiento en el mercado.</p>
+          </div>
+        </div>
+
+      </div>
+    </section><!-- End Services Section -->
 @endsection
