4d9166811bbdb05edd1dd731d283561bd099c27b
Cristi0009 <cristina-calero@hotmail.com>
2022-02-24 20:33:24 -0500

    [UPDATE] add mission objetives vision.blade.php

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -4,40 +4,23 @@
 @endsection
 @section('main')
 
-     <!-- ======= About Us Section ======= -->
-     <section id="about" class="about">
+    <!-- ======= Mission ======= -->
+<section id="about" class="about">
       <div class="container" data-aos="fade-up">
         <div class="row no-gutters">
           <div class="col-lg-6 video-box">
-            <img src="assets/img/about.jpg" class="img-fluid" alt="">
-            <!-- <a href="https://www.youtube.com/watch?v=jDDaplaOz7Q" class="venobox play-btn mb-4" data-vbtype="video" data-autoplay="true"></a> -->
+            <img src="assets/img/mission2.jpg" class="img-fluid" alt="">
           </div>
 
           <div class="col-lg-6 d-flex flex-column justify-content-center about-content">
 
             <div class="section-title">
-              <h2>Nosotros</h2>
-              <p>Somos una empresa dedicada al desarrollo de software con más de 5 años en el mercado tecnológico, brindando soluciones web
-                creativas a empresas y personas interesadas en tener una presencia activa en internet.</p>
-                <br>
-                <p>Nuestro objetivo es proporcionar soluciones
-                  informáticas y de gestión adecuadas a cada
-                  cliente, usando tecnologías y metodologías
-                  avanzadas.</p>
+              <h2>Misión</h2>             
             </div>
 
-            <div id="vision" class="icon-box" data-aos="fade-up" data-aos-delay="100">
-              <div class="icon"><i class="bx bx-briefcase"></i></div>
-              <h4 class="title"><a href="">VISIÓN</a></h4>
-              <p class="description">Ser una empresa líder en el desarrollo de soluciones tecnológicas innovadoras a nivel
-                provincial y nacional cumpliendo con los estándares de calidad de software, fomentando las últimas tendencias tecnológicas y
-                lograr la satisfacción de nuestros clientes.</p>
-            </div>
-
-            <div id="mision" class="icon-box" data-aos="fade-up" data-aos-delay="100">
+            <div class="icon-box" data-aos="fade-up" data-aos-delay="100">
               <div class="icon"><i class="bx bx-building"></i></div>
-              <h4 class="title"><a href="">MISIÓN</a></h4>
-              <p class="description">JIVSOFT es una empresa orientada a las
+              <p class="description">JIVSoft es una empresa orientada a las
                 soluciones informáticas comprometida con
                 el desarrollo de software de alta calidad, para
                 satisfacer las necesidades de pequeñas y
@@ -46,10 +29,86 @@
                 productividad.</p>
             </div>
 
+          </div>
+        </div>
+
+      </div>
+    </section><!-- End Mission Section -->
+
+
+<!-- ======= Vision Section ======= -->
+<section id="about" class="about">
+      <div class="container" data-aos="fade-up">
+        <div class="row no-gutters">
+          <div class="col-lg-6 video-box">
+            <img src="assets/img/vision1.jpg" class="img-fluid" alt="">
+          </div>
+
+          <div class="col-lg-6 d-flex flex-column justify-content-center about-content">
+
+            <div class="section-title">
+              <h2>Visión</h2>             
+            </div>
+
+            <div class="icon-box" data-aos="fade-up" data-aos-delay="100">
+              <div class="icon"><i class="bx bx-briefcase"></i></div>
+              <p class="description">Ser una empresa líder en el desarrollo de soluciones tecnológicas innovadoras a nivel
+                provincial y nacional cumpliendo con los estándares de calidad de software, fomentando las últimas tendencias tecnológicas y
+                lograr la satisfacción de nuestros clientes.</p>
+            </div>
 
           </div>
         </div>
 
       </div>
-    </section><!-- End About Us Section -->
+    </section><!-- End Vision Section -->
+
+
+<!-- ======= Objectives Section ======= -->
+<section id="about" class="about">
+      <div class="container" data-aos="fade-up">
+        <div class="row no-gutters">
+          <div class="col-lg-6 video-box">
+            <img src="assets/img/about.jpg" class="img-fluid" alt="">
+          </div>
+
+          <div class="col-lg-6 d-flex flex-column justify-content-center about-content">
+
+            <div class="section-title">
+              <h2>Objetivos</h2>             
+            </div>
+
+            <div class="icon-box" data-aos="fade-up" data-aos-delay="100">
+              <div class="icon"><i class="bx bx-sun"></i></div>
+              <p class="description">Ofrecer soluciones tecnológicas
+                  innovadoras en la automatización
+                  de sus procesos de negocio.</p>
+            </div>
+
+            <div class="icon-box" data-aos="fade-up" data-aos-delay="100">
+              <div class="icon"><i class="bx bx-briefcase"></i></div>
+              <p class="description">Brindar nuestros conocimientos 
+                   y experiencia al servicio de
+                  nuestros clientes.</p>
+            </div>
+
+            <div class="icon-box" data-aos="fade-up" data-aos-delay="100">
+              <div class="icon"><i class="bx bx-list-check"></i></div>
+              <p class="description">Aplicar los estándares de
+                  calidad en el desarrollo de
+                  software.</p>
+            </div>
+
+            <div class="icon-box" data-aos="fade-up" data-aos-delay="100">
+              <div class="icon"><i class="bx bx-stopwatch"></i></div>
+              <p class="description">Acompañar antes, durante y después
+                  de la transformación empresarial y
+                  tecnológica de su negocio.</p>
+            </div>
+
+          </div>
+        </div>
+
+      </div>
+    </section><!-- End Objetive Section -->
 @endsection
