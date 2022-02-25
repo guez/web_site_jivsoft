31800c9006a8b31192659076b2fe213e4fee08d1
Cristi0009 <cristina-calero@hotmail.com>
2022-02-25 18:34:04 -0500

    [UPDATE] information contact.blade.php

--- resources/views/pages/contact.blade.php
+++ resources/views/pages/contact.blade.php
@@ -35,27 +35,36 @@
           </div>
 
           <div class="col-lg-12" data-aos="fade-up" data-aos-delay="300">
-            <form action="forms/contact.php" method="post" role="form" class="php-email-form">
+            <form action="/contactRegister" method="POST" class="php-email-form">
+              @csrf
               <div class="row">
                 <div class="col-lg-6 form-group">
-                  <input type="text" class="form-control" name="Nombre" id="Nombre" placeholder="Nombre" required>
+                  <input type="text" class="form-control" name="name" id="name" placeholder="Nombre" required>
                 </div>
                 <div class="col-lg-6 form-group">
-                  <input type="email" class="form-control" name="Correo electrónico" id="Correo electrónico" placeholder="Correo electrónico" required>
+                  <input type="text" class="form-control" name="enterprise" id="enterprise" placeholder="Nombre de la Empresa">
+                </div>
+                <div class="col-lg-6 form-group">
+                  <input type="email" class="form-control" name="email" id="email" placeholder="Correo electrónico" required>
+                </div>
+                <div class="col-lg-6 form-group">
+                  <input type="phone" class="form-control" name="phone" id="phone" placeholder="Teléfono/Celular" required>
                 </div>
               </div>
               <div class="form-group">
-                <input type="text" class="form-control" name="Asunto" id="Asunto" placeholder="Asunto" required>
+                <input type="text" class="form-control" name="subject" id="subject" placeholder="Asunto" required>
               </div>
               <div class="form-group">
-                <textarea class="form-control" name="Mensaje" rows="5" placeholder="Mensaje" required></textarea>
+                <textarea class="form-control" name="message" rows="5" placeholder="Mensaje" required></textarea>
               </div>
               <div class="my-3">
                 <div class="loading">Cargando</div>
                 <div class="error-message"></div>
                 <div class="sent-message">Tu mensaje ha sido enviado, Gracias!</div>
               </div>
-              <div class="text-center"><button type="submit">Enviar</button></div>
+              <div class="text-center">
+                <button type="submit">Enviar</button>
+              </div>
             </form>
           </div>
 
