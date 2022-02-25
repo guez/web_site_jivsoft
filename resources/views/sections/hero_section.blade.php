1ab2ded004ca4e79a209afe8723726717bd5f1f8
Cristi0009 <cristina-calero@hotmail.com>
2022-02-24 22:17:57 -0500

    [UPDATE]  information carrusel.blade.php

--- resources/views/sections/hero_section.blade.php
+++ resources/views/sections/hero_section.blade.php
@@ -11,12 +11,12 @@
           <div class="carousel-item active" style="background-image: url('assets/img/slide/slide-1.jpg');">
             <div class="carousel-container">
               <div class="carousel-content container">
-                < <h2 class="animate__animated animate__fadeInDown">Bienvenido a <span>JIVSOFT</span></h2>
+                < <h2 class="animate__animated animate__fadeInDown">Bienvenido a <span>JIVSoft</span></h2>
                 <p class="animate__animated animate__fadeInUp">Somos una empresa dedicada al desarrollo
                   de software con más de 5 años en el mercado tecnológico, brindando soluciones web
                   creativas a empresas y personas interesadas
-                  en tener una presencia activa en internet</p>
-                <a href="#about" class="btn-get-started animate__animated animate__fadeInUp scrollto">Leer Mas</a>
+                  en tener una presencia activa en internet.</p>
+                <a href="mission" class="btn-get-started animate__animated animate__fadeInUp scrollto">Leer Mas</a>
               </div>
             </div>
           </div>
@@ -25,10 +25,11 @@
           <div class="carousel-item" style="background-image: url('assets/img/slide/slide-2.jpg');">
             <div class="carousel-container">
               <div class="carousel-content container">
-                <h2 class="animate__animated animate__fadeInDown">Páginas Web</h2>
-                <p class="animate__animated animate__fadeInUp">En la actualidad, es impres- cindible que una empresa u organización tenga presencia en la web.
-                 Jivsoft brinda a sus clientes la mejor experiencia con una atractiva aplicación web, desde la planificación y diseño de su sitio web, hasta la creación y mantenimiento del mismo.</p>
-                <a href="#about" class="btn-get-started animate__animated animate__fadeInUp scrollto">Leer Mas</a>
+                <h2 class="animate__animated animate__fadeInDown">Conoce nuestros servicios</h2>
+                <p class="animate__animated animate__fadeInUp">Nuestros servicios están diseñados para cumplir sus objetivos corporativos de una manera
+                          más eficiente, optimizando recursos y automatizando procesos que lo llevarán a alcanzar una
+                          mayor competitividad en el mercado.</p>
+                <a href="services" class="btn-get-started animate__animated animate__fadeInUp scrollto">Leer Mas</a>
               </div>
             </div>
           </div>
@@ -37,9 +38,12 @@
           <div class="carousel-item" style="background-image: url('assets/img/slide/slide-3.jpg');">
             <div class="carousel-container">
               <div class="carousel-content container">
-                <h2 class="animate__animated animate__fadeInDown">Diseño Responsive</h2>
-                <p class="animate__animated animate__fadeInUp">Es una técnica donde de diseño web que tiene como objetivo proporcionar una de visualización óptima a través de una amplia gama de dis- positivos. Desde ordenado- res de escritorio a tablets y móviles.</p>
-                <a href="#about" class="btn-get-started animate__animated animate__fadeInUp scrollto">Leer Mas</a>
+                <h2 class="animate__animated animate__fadeInDown">Revisa nuestro portafolio</h2>
+                <p class="animate__animated animate__fadeInUp">Si algo garantiza los
+                          resultados de nuestro trabajo es nuestra alta responsabilidad en la gestión de 
+                          los proyectos, con una planificación y control constantes, siguiendo metodologías 
+                          reconocidas y tomando como base los estándares de calidad de software.</p>
+                <a href="briefcase" class="btn-get-started animate__animated animate__fadeInUp scrollto">Portafolio</a>
               </div>
             </div>
           </div>
