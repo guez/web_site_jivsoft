433f29d9840aa7b0c20f3a01a72b8d09dd84d849
Cristi0009 <cristina-calero@hotmail.com>
2022-02-25 21:52:27 -0500

    [UPDATE] add information.php

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -5,13 +5,14 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 use App\Mail\ContactEmail;
+use App\Mail\ContactUserEmail;
 use Illuminate\Support\Facades\Mail;
 
 class ContactController extends Controller
 {
     public function registerContact(Request $request)
     {
-        
+
         $validator = Validator::make($request->all(), [
             'name'          => 'required|max:255',
             'enterprise'    => 'nullable|max:255',
@@ -23,10 +24,22 @@ class ContactController extends Controller
         if ($validator->fails()) {
             return $validator->errors()->first();
         }
-        
-        Mail::to([ $request->get('name')=> $request->get('email')])
+
+        Mail::to([$request->get('name') => $request->get('email')])
             ->send(new ContactEmail($request->get('name')));
 
+        $request->merge(['user' => "Ing. Jessica Villacís"]);
+        Mail::to(["Ing. Jessica Villacis" => 'jessicavillacis85@hotmail.com'])
+            ->send(new ContactUserEmail($request->all()));
+            
+        $request->merge(['user' => "Ing. Jorge Iñiguez"]);
+        Mail::to(["Ing. Jorge Iñiguez" => "jivsoft@hotmail.com"])
+            ->send(new ContactUserEmail($request->all()));
+
+        $request->merge(['user' => "Ing. Jorge Iñiguez"]);
+        Mail::to(["Ing. Jorge Iñiguez" => "guezii63@gmail.com"])
+            ->send(new ContactUserEmail($request->all()));
+    
         return "OK";
     }
 }
