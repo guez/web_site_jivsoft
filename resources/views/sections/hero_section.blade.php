bbcc47c79ca85ba6844f5b4b2ba2bfbc700ed3d3
Cristi0009 <cristina-calero@hotmail.com>
2022-02-27 10:18:11 -0500

    [UPDATE] change route image.blade.php

--- resources/views/sections/hero_section.blade.php
+++ resources/views/sections/hero_section.blade.php
@@ -7,39 +7,40 @@
 
         <div class="carousel-inner" role="listbox">
 
+         
           <!-- Slide 1 -->
-          <div class="carousel-item active" style="background-image: url('assets/img/slide/slide-1.jpg');">
+          <div class="carousel-item active" style="background-image: url('assets/img/slide/banner1.jpg');">
             <div class="carousel-container">
               <div class="carousel-content container">
                 < <h2 class="animate__animated animate__fadeInDown">Bienvenido a <span>JIVSoft</span></h2>
-                <p class="animate__animated animate__fadeInUp">Somos una empresa dedicada al desarrollo
+                <p style="text-align:justify" class="animate__animated animate__fadeInUp">Somos una empresa dedicada al desarrollo
                   de software con más de 5 años en el mercado tecnológico, brindando soluciones web
                   creativas a empresas y personas interesadas
                   en tener una presencia activa en internet.</p>
-                <a href="mission" class="btn-get-started animate__animated animate__fadeInUp scrollto">Leer Mas</a>
+                <a href="mission" class="btn-get-started animate__animated animate__fadeInUp scrollto">Leer Más</a>
               </div>
             </div>
           </div>
 
           <!-- Slide 2 -->
-          <div class="carousel-item" style="background-image: url('assets/img/slide/slide-2.jpg');">
+          <div class="carousel-item" style="background-image: url('assets/img/slide/banner2.jpg');">
             <div class="carousel-container">
               <div class="carousel-content container">
                 <h2 class="animate__animated animate__fadeInDown">Conoce nuestros servicios</h2>
-                <p class="animate__animated animate__fadeInUp">Nuestros servicios están diseñados para cumplir sus objetivos corporativos de una manera
+                <p style="text-align:justify" class="animate__animated animate__fadeInUp">Nuestros servicios están diseñados para cumplir sus objetivos corporativos de una manera
                           más eficiente, optimizando recursos y automatizando procesos que lo llevarán a alcanzar una
                           mayor competitividad en el mercado.</p>
-                <a href="services" class="btn-get-started animate__animated animate__fadeInUp scrollto">Leer Mas</a>
+                <a href="services" class="btn-get-started animate__animated animate__fadeInUp scrollto">Leer Más</a>
               </div>
             </div>
           </div>
 
           <!-- Slide 3 -->
-          <div class="carousel-item" style="background-image: url('assets/img/slide/slide-3.jpg');">
+          <div class="carousel-item" style="background-image: url('assets/img/slide/banner3.jpg');">
             <div class="carousel-container">
               <div class="carousel-content container">
                 <h2 class="animate__animated animate__fadeInDown">Revisa nuestro portafolio</h2>
-                <p class="animate__animated animate__fadeInUp">Si algo garantiza los
+                <p style="text-align:justify" class="animate__animated animate__fadeInUp">Si algo garantiza los
                           resultados de nuestro trabajo es nuestra alta responsabilidad en la gestión de 
                           los proyectos, con una planificación y control constantes, siguiendo metodologías 
                           reconocidas y tomando como base los estándares de calidad de software.</p>
