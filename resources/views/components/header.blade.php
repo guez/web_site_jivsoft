d6c66a9727c995fb9c0a71eeede003d804538ec2
Cristi0009 <cristina-calero@hotmail.com>
2022-03-04 23:30:48 -0500

    [UPDATE] change information header.blade.php

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -3,7 +3,7 @@
     <div class="container d-flex align-items-center">
 
       <div class="logo me-auto">
-        <img src="/assets/img/logo4.jpg" >
+        <img src="/assets/img/logo.png" >
         <!-- Uncomment below if you prefer to use an image logo -->
         <!-- <a href="index.html"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
       </div>
@@ -12,14 +12,14 @@
       <nav id="navbar" class="navbar">
         <ul>
           <li><a class="nav-link scrollto active" href="/">Inicio</a></li>
-          <li><a class="nav-link scrollto" href="/briefcase">Portfolio</a></li>
+          <li><a class="nav-link scrollto" href="/briefcase">Portafolio</a></li>
           <li><a class="nav-link scrollto" href="/services">Servicios</a></li>
-          <li><a class="nav-link scrollto" href="#team">Equipo</a></li>
+          <li><a class="nav-link scrollto" href="/team">Equipo</a></li>
           <li class="dropdown"><a href="#"><span>Nosotros</span> <i class="bi bi-chevron-down"></i></a>
             <ul>
               <li><a href="vision">Visión</a></li>
               <li><a href="mission">Misión</a></li>
-              <li><a href="objetives">Objetivos</a></li>
+              <li><a href="objectives">Objetivos</a></li>
             </ul>
           </li>
           <li><a class="nav-link scrollto" href="/contactos">Contactos</a></li>
