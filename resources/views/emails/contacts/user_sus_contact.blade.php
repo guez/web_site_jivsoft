bb0920bc5d6536b77d78c6cc297c371cd3755b12
Cristi0009 <cristina-calero@hotmail.com>
2022-03-14 13:21:42 -0500

    [CREATE] user_sus_contact.blade.php

---
+++ resources/views/emails/contacts/user_sus_contact.blade.php
@@ -0,0 +1,16 @@
+<br>
+<p>
+    Hola {{ $data['user'] }}.
+</p>
+
+<p>
+    JIVSoft tienes un nuevo nuevo suscriptor, su correo es "{{ $data['email'] }}".
+</p>
+<br>
+<br>
+<p>
+    Att:
+</p>
+<p>
+    JIVSoft Enterprise.
+</p>
