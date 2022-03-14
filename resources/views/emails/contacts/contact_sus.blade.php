dc14c308f66c64fa864b4333baeb8f9cafcf0897
Cristi0009 <cristina-calero@hotmail.com>
2022-03-14 13:20:19 -0500

    [CREATE] contact_sus.blade.php

---
+++ resources/views/emails/contacts/contact_sus.blade.php
@@ -0,0 +1,18 @@
+<br>
+<p>
+    Hola {{ $email }}, bienvenido.
+</p>
+
+<p>
+    JIVSoft recibió tu correo electrónico, gracias por suscribirte.
+</p>
+<br>
+<br>
+<p>
+    Att:
+</p>
+<br>
+
+<p>
+    JIVSoft.
+</p>
