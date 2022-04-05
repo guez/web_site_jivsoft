7104c1e0162deaa7ee34fad6994313252a7d53b4
Jorge <guez_95ii@hotmail.com>
2022-04-04 19:15:49 -0500

    [ADD] Middleware\VisitsMiddleware.php

---
+++ app/Http/Middleware/VisitsMiddleware.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use App\Models\Visit;
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+class VisitsMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        $route = Route::getCurrentRoute();
+
+        $requestHost = new Request([
+            'host' =>   $request->header('host'),
+            'userAgent'  => $request->header('user-agent'),
+            'url'  =>   $route->uri(),
+        ]);
+
+        Visit::storeRecord($requestHost);
+
+        return $next($request);
+    }
+}
